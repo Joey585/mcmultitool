0ad96e1c0aecb7b4784df68c5e33fe15cdf4fea6
Joey585 <thelich06@protonmail.com>
2021-04-28 18:05:12 -0700

    login

--- mcmultitool.bat
+++ mcmultitool.bat
@@ -1,5 +1,14 @@
 @echo off
+color 2
+cls
 
-echo First edition
-
-// joey585 edit test LOLOLOLL
+:login
+echo                                  _ _   _ _              _ 
+echo                                 | | | (_) |            | |
+echo   _ __ ___   ___ _ __ ___  _   _| | |_ _| |_ ___   ___ | |
+echo  | '_ ` _ \ / __| '_ ` _ \| | | | | __| | __/ _ \ / _ \| |
+echo  | | | | | | (__| | | | | | |_| | | |_| | || (_) | (_) | |
+echo  |_| |_| |_|\___|_| |_| |_|\__,_|_|\__|_|\__\___/ \___/|_|
+echo.
+set /p user=Please enter your username: 
+set /p pass=Please enter your password:
